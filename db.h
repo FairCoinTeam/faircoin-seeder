a6837e46e6918d4ea3cc918684f8a275fe8150ec
thokon00 <tom@fair-coin.org>
2015-03-20 10:00:54 +0100

    UPDATED: new required block height

--- db.h
+++ db.h
@@ -16,7 +16,7 @@
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 0 : 94613;
+    return testnet ? 0 : 102079;
 }
 
 std::string static inline ToString(const CService &ip) {
